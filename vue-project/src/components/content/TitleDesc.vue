ebd94ed9d88941196b1d052686632495253c7a0a
34 <34>
2023-10-19 13:38:02 +0300

    push

--- vue-project/src/components/content/TitleDesc.vue
+++ vue-project/src/components/content/TitleDesc.vue
@@ -1,30 +1,15 @@
 <template>
-  <h3 class="  text-center  text-4xl " :class="[variant]">{{ title }}</h3>
-  <p class=" my-2 m mx-1 text-lg  text-gray-800" :class="[variantDesc]">{{ desc }}</p>
+  <h3 class="text-center text-4xl" :class="variant">{{ title }}</h3>
+  <p class="my-2 mx-1 mb-4 text-lg text-gray-800" :class="variantDesc">{{ desc }}</p>
 </template>
+
 <script>
 export default {
   props: {
     title: String,
     desc: String,
-    variant: {
-      default: 'text-4xl text-start ',
-      validator(value) {
-        return [
-          'text-2xl text-black  text-start text-bold',
-
-        ].includes(value)
-      }
-    },
-    variantDesc: {
-      default: 'text-lg text-start ',
-      validator(value) {
-        return [
-          'text-2xl text-yellow-700 text-center',
-        ].includes(value)
-      }
-
-    }
+    variant: String,
+    variantDesc: String,
   }
 }
 </script>
--- vue-project/src/components/core/InputFeild.vue
+++ vue-project/src/components/core/InputFeild.vue
@@ -1,15 +1,18 @@
 <template>
-    <label>{{ label }}</label>
-    <input type="text"
-        class="block w-full mt-3 p-4 pl-5 text-sm text-gray-900 border   	focus:outline-none  bg-white     focus:border-violet-700 "
-        :placeholder="placeholder" required>
+    <div>
+        <label>{{ label }}</label>
+        <input type="text"
+            class="block w-full mt-3 p-4 pl-5 text-sm text-gray-900 border focus:outline-none bg-white focus:border-violet-700"
+            :placeholder="placeholder" required />
+    </div>
 </template>
+  
 <script>
 export default {
     props: {
         placeholder: String,
         label: String
-
     }
 }
 </script>
+  
